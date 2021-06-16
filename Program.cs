ea3d56870ae41c38523a90e31daf61b8c4849a76
Саня <sanya@Sergejs-MacBook-Air.local>
2021-06-16 17:47:29 +0600

    Сделано первое задание: в массиве из 18 элементов найти сумму всех положительных элементов

--- Program.cs
+++ Program.cs
@@ -6,7 +6,30 @@ namespace Option5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Вариант 5
+            //?А) В массиве из 18 элементов найти сумму всех положительных элементов.
+
+            int arraySize = 18;
+            int[] array = new int[arraySize];
+            Random rand = new Random();
+            int sum = 0;
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                array[i] = rand.Next(100) - 40;
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                if (array[i] >= 0)
+                {
+                    sum += array[i];
+                }
+            }
+
+            Console.WriteLine($"Сумма положительных элементов массива = {sum}");
         }
     }
 }
