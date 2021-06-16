04295f8552249b8d3806a2a4881938b82f6357d0
Саня <sanya@Sergejs-MacBook-Air.local>
2021-06-16 18:12:00 +0600

    Сделаны задания 2 и 3

--- Program.cs
+++ Program.cs
@@ -30,6 +30,56 @@ namespace Option5
             }
 
             Console.WriteLine($"Сумма положительных элементов массива = {sum}");
+
+
+            //?Б) Дан массив из 30 элементов. Найти порядковый номер первого отрицательного элемента.
+
+            int arrSize = 30;
+            int[] arr = new int[arrSize];
+
+            for (int i = 0; i < arrSize; i++)
+            {
+                arr[i] = rand.Next(100) - 40;
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                if (arr[i] < 0)
+                {
+                    Console.WriteLine($"Порядковый номер первого отрицательного элемента массива = {i + 1}");
+                    break;
+                }
+            }
+
+            //В) Переменной t присвоить значение true, если в массиве x нет нулевых элементов, и при
+            //этом отрицательные элементы чередуются с положительными, и значение – false в
+            //противном случае.
+
+            int xSize = 10;
+            //int[] x = new int[xSize]{ 2, -5, 44, -27, 16, -16, 9, -10, 1, 25 };
+            int[] x = { 2, -5, 44, -27, 16, -16, 9, -10, 1, 25 };
+            bool t = true;
+
+            for (int i = 0; i < x.Length - 1; i = i + 2)
+            {
+                if (x[i] > 0 && x[i + 1] < 0)
+                {
+                    if (x[i] == 0)
+                    {
+                        t = true;
+                    }
+
+                }
+                else
+                {
+                    t = false;
+                    break;
+                }
+            }
+            Console.WriteLine($"t = {t}");
+
         }
     }
 }
